883f8040e6a5b5d38522da1b2609bc9eb2f9dd51
lee <ehdqn119@gmail.com>
2023-02-22 05:49:44 +0900

    color change

--- src/component/core/route-breadcrumbs.js
+++ src/component/core/route-breadcrumbs.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate, useLocation   } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 
 
 import { useDispatch } from 'react-redux'
 import { addPath, removePath } from "~features/route/route-slice";
 import Box from '@mui/material/Box';
-
+import Typography from '@mui/material/Typography';
+import { palette } from "@mui/system";
 
 const RouteBreadcrumbs = () => {
-  
+
   const breadcrumbs = useBreadcrumbs();
   const navigate = useNavigate();
   const location = useLocation()
@@ -20,47 +21,16 @@ const RouteBreadcrumbs = () => {
 
 
   return (
-    <Box className="breadcrumbs" sx={{ display : 'flex', alignItems : 'center'}}>
+    <Box className="breadcrumbs" sx={{ display: 'flex', alignItems: 'center' }}>
       {breadcrumbs.map(({ breadcrumb, match }, index) => (
-        <div className="bc" key={match.pathname}>
-          <Link to={match.pathname || ""}>{breadcrumb}</Link>
-          {index < breadcrumbs.length - 1 && " >" + " "}
-        </div>
+        <Box className="bc" key={match.pathname}>
+          <Box component={Link} to={match.pathname || ""}>
+            <Typography color='textPrimary' >{breadcrumb} {index < breadcrumbs.length - 1 && " >" + " "}</Typography>
+            </Box>
+        </Box>
       ))}
     </Box>
   );
 };
 
 export default RouteBreadcrumbs;
-
-
-/*
-const location = useLocation();
-  // 필요없을 수도 있습니다.
-  const pathnames = location.pathname.split('/').filter((x) => x);
-
-  return (
-    
-  );
-<Breadcrumbs aria-label="breadcrumb">
-      <LinkRouter underline="hover" color="inherit" to="/">
-        <Typography variant='body1' color="text.primary">Home</Typography>
-      </LinkRouter>
-      {pathnames.map((value, index) => {
-        const last = index === pathnames.length - 1;
-        const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-
-        return last ? (
-          <Typography color="text.primary" key={to}>
-            {breadcrumbNameMap[to]}
-          </Typography>
-        ) : (
-          <LinkRouter underline="hover" color="inherit" to={to} key={to}>
-            {breadcrumbNameMap[to]}
-          </LinkRouter>
-        );
-      })}
-    </Breadcrumbs>
-
-
-*/
